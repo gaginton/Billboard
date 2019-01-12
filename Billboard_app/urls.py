07b2341888af60481d12041b5ee63b354315e4ce
Guy Ginton <guyginton@gmail.com>
2019-01-12 17:37:19 +0200

    some fixes

--- Billboard_app/urls.py
+++ Billboard_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
+    # url(r'^login$', 'django.contrib.auth.views.login'),
     url(r'^register$', views.register, name='register'),
     url(r'^home$', views.home, name='home'),
     url(r'^home/add_bb$', views.add_bb, name='Add New Billboard')
